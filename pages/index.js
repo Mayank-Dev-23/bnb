259992aeb09fce76f931a9f2f1cfd8e757361ee7
Mayank Saxena <saxenamayank899@gamil.com>
2021-08-06 14:01:15 +0530

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Header from '../Components/Header'
+
+export default function Home() {
+  return (
+    <div >
+      <Head>
+        <title>Airbnb</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+
+      <Header />
+
+    </div>
+  )
+}
