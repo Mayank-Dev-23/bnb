d93e5bc8e422f46173ef040112bb136376ecf8a2
Mayank Saxena <saxenamayank899@gamil.com>
2021-08-06 14:30:25 +0530

    iniital commit

--- Components/Header.js
+++ Components/Header.js
@@ -3,35 +3,21 @@ import {GlobeAltIcon, MenuIcon, SearchIcon, UserCircleIcon} from "@heroicons/rea
 
 function Header() {
   return (
-    <header className="p-3 sticky z-50 top-0 shadow-md bg-white  grid grid-cols-3 md:px-10">
-      {/* left */}
-      <div className="relative h-10 items-center cursor-pointer my-auto">
-        <Image
-          src="https://links.papareact.com/qd3"
-          layout="fill"
-          objectFit="contain"
-          objectPosition="left"
-        />
-      </div>
-
-      {/* center */}
-
-      <div className="flex  items-center border-2 rounded-full py-2 md:shadow-sm">
-          <input className="flex-grow pl-4 bg-transparent outline-none text-gray-600 placeholder-gray-400" type="text" placeholder="Start your search" />
-          <SearchIcon className=" hidden md:flex md:mx-2 h-8 bg-red-400 rounded-full p-2 cursor-pointer text-white" />
-      </div>
-
-      {/* right */}
-        <div className="flex space-x-2 items-center justify-end text-gray-500">
-            <p className="hidden md:flex">Become a Host</p>
-            <GlobeAltIcon className="h-6 cursor-pointer" />
-
-<div className="flex items-center space-x-2 border-2 p-1 rounded-full">
-   <MenuIcon  className="h-6"/>
-   <UserCircleIcon className="h-6" />
-</div>
-
-        </div>
+    <header className="shadow-md py-3 px-2  grid grid-cols-3 items-center" >
+  
+ 
+ <div className="relative h-10">
+     <Image src="https://links.papareact.com/qd3" 
+     layout="fill"
+     objectPosition="left"
+     objectFit="contain"
+     
+     />
+ </div>
+ <div >
+     <GlobeAltIcon className="h-7 cursor-pointer" />
+ </div>
+   
     </header>
   );
 }
